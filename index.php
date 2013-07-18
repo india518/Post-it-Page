2a555e7ebfd321ad34b29fd30b762017860624ca
India Meisner <india.rae@gmail.com>
2013-07-17 17:21:37 -0700

    Form textarea now clears on asynchronous submit. Also, CSS fix for height of notes varying with note length (changed from <display: inline-block> to <float: left> on all divs

--- index.php
+++ index.php
@@ -15,22 +15,21 @@
 		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function(){
-
 				$("#create_note").submit(function(){
 					$.post(
 						$(this).attr("action"), //URL to send data
 						$(this).serialize(), //the data being sent
 						function(data){
-							//if (pageLoad)
-								//$("#note_container").html(data);
-							//else
-								$("#note_container").append(data);
+							$("#note_container").append(data);
 						},
 						"json"	//data format
 					);
+					//$(this).reset(); --> Doesn't work because it's a jQuery object
+					this.reset(); //"This" is a javascript reference to this
+					// Another way to do the above command:
+					// document.getElementById("create_note").reset();
 					return false;
 				});
-
 			});
 		</script>
 	</head>
@@ -43,11 +42,11 @@
 			</div>
 			<div class="clear"></div>
 			<form id="create_note" action="process.php" method="post">
-				<p><label for="note_description">Add a Note:</label></p>
-				<p><textarea id="note_description" name="note_description"></textarea></p>
+				<!-- I don't wan to use a <br />, since that's bad practice -->
+				<div><label for="note_description">Add a Note:</label></div>
+				<div><textarea id="note_description" name="note_description"></textarea></div>
 				<input type="submit" value="Post It!" />
 			</form>
 		</div>
-
 	</body>
 </html>
