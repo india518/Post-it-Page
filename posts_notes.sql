cc0a3ab6668ac826918229a699c8fd15f0406681
India Meisner <india.rae@gmail.com>
2013-07-17 20:30:33 -0700

    Updated database to load and play with

--- posts_notes.sql
+++ posts_notes.sql
@@ -1,27 +1,55 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE DATABASE  IF NOT EXISTS `post_notes` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `post_notes`;
+-- MySQL dump 10.13  Distrib 5.5.24, for osx10.5 (i386)
+--
+-- Host: 127.0.0.1    Database: post_notes
+-- ------------------------------------------------------
+-- Server version	5.5.29
 
-DROP SCHEMA IF EXISTS `post_notes` ;
-CREATE SCHEMA IF NOT EXISTS `post_notes` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `post_notes` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `post_notes`.`posts`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `post_notes`.`posts` ;
+--
+-- Table structure for table `posts`
+--
 
-CREATE  TABLE IF NOT EXISTS `post_notes`.`posts` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `description` TEXT NULL ,
-  `created_at` DATETIME NULL ,
-  `updated_at` DATETIME NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-USE `post_notes` ;
+--
+-- Dumping data for table `posts`
+--
 
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'Here is a note!','2013-07-17 17:09:33',NULL),(2,'Here is a longer note. What happens when we have a long note?','2013-07-17 17:09:49',NULL),(3,'I wonder why the height of my notes change depending on the length of my note...','2013-07-17 17:10:26',NULL),(4,'You know what would be fun? Baking cookies!','2013-07-17 17:10:56',NULL),(5,'I like cookies.','2013-07-17 17:11:01',NULL),(6,'Here is another test.','2013-07-17 17:17:56',NULL),(7,'And it still seems to work','2013-07-17 17:18:04',NULL),(8,'Yay!','2013-07-17 17:18:17',NULL);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-07-17 17:50:23
